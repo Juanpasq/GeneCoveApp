294f01bac4cb4903bf24e47eb408e1dea11cfa96
Juanpasq <35710286+Juanpasq@users.noreply.github.com>
2018-04-15 20:28:56 -0300

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -1,83 +1,64 @@
 
-$(document).ready(function() {
-          
-     // alert("document ready - prueba de carga document ready!");
+$(document).ready(function () {
 
-////// funcion para leer un archivo via remoto http ////////
-    
-    //function probarBtn (){
-        
-     //   alert("Boton clickeado");
-   // };
-    
-    
-    
-    
-    $(function() 
-    {
-        $("#button").click( function()
-        {
-           alert('boton clickeado');
-        //       var TXT_URL = 'https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt';
-           var TXT_URL = $("#input-url").val();
+    // alert("document ready - prueba de carga document ready!");
+
+    ////// funcion para leer un archivo via remoto http ////////
+
+    //function probarBtn (){  
+    //   alert("Boton clickeado");
+    // };
+
+    $(function () {
+        $("#button").click(function () {
+            alert('boton clickeado');
+            //       var TXT_URL = 'https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt';
+            var TXT_URL = $("#input-url").val();
 
             $.ajax
-            (
+                (
                 {
-                    url : TXT_URL,
+                    url: TXT_URL,
                     dataType: "text",
-                    success : function (data) 
-                    {
-                        $(".text").html("<pre>"+data+"</pre>");
+                    success: function (data) {
+                        $(".text").html("<pre>" + data + "</pre>");
                     }
                 }
-            );
-       });
+                );
+        });
     });
-    
-
-////// FIN funcion para leer un archivo via remoto http ////////
 
 
-//// Leer un archivo TXT Local /////////
- 
-  
-        
-        function leerArchivo(e) {
-          var archivo = e.target.files[0];
-          if (!archivo) {
+    //// Leer un archivo TXT Local /////////
+
+    function leerArchivo(e) {
+        var archivo = e.target.files[0];
+        if (!archivo) {
             return;
-          }
-          var lector = new FileReader();
-          lector.onload = function(e) {
+        }
+        var lector = new FileReader();
+        lector.onload = function (e) {
             var contenido = e.target.result;
             mostrarContenido(contenido);
-          };
-            
-          lector.readAsText(archivo);
-        }
+        };
 
-        function mostrarContenido(contenido) {
-          var elemento = document.getElementById('input-csv');
-          elemento.innerHTML = contenido;
-            alert("File content:  "+contenido);
-            document.getElementById('input-csv').value = contenido;
-            
-        }
+        lector.readAsText(archivo);
+    }
 
-   
-       document.getElementById('file-input')
-          .addEventListener('change', leerArchivo, false); 
-       
-    
+    function mostrarContenido(contenido) {
+        var elemento = document.getElementById('input-csv');
+        elemento.innerHTML = contenido;
+        alert("File content:  " + contenido);
+        document.getElementById('input-csv').value = contenido;
 
-//////// FIN Leer archivo TXT Local //////////
+    }
 
-    
-    
-    
+    document.getElementById('file-input')
+        .addEventListener('change', leerArchivo, false);
+
+
 }); ////cierre documentReady////////  
-  
+
 
 
 
