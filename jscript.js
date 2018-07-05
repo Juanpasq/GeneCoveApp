88e49f7b8dff22d640b03037466bee9f73aa96cd
juanpasq <manu_pasquali@hotmail.com>
2018-07-05 17:50:48 -0300

    alert - validaciones vacios

--- jscript.js
+++ jscript.js
@@ -1,63 +1,79 @@
+
+////////botones close de alert /////////
+function closeAlerGen(){
+    
+    var a = document.getElementById("alertGen");
+    a.style.display = "none";
+}
+
+function closeAlerKit(){
+    
+    var b = document.getElementById("alertKit");
+    b.style.display = "none";
+}
+
+
 $(document).ready(function () {
-    
-    
-    
- 
+
+    ///toolip diseño globo///
+    $('[data-toggle="tooltip"]').tooltip();   
+
+
     ////////traer el json de kitnames/////////// 
-        var ipServer = "10.100.100.55:8081";
-        $.getJSON("http://"+ipServer+"/genecovapp/getAllKit", function(kitjson) {
+    var ipServer = "10.100.100.55:8081";
+    $.getJSON("http://" + ipServer + "/genecovapp/getAllKit", function (kitjson) {
 
-                    for (i = 0; i < kitjson.length; i++) {
+        for (i = 0; i < kitjson.length; i++) {
 
-                        var valorKitname = kitjson[i].id;
-                        var kitname = kitjson[i].kitname;
-                        var x = document.getElementById("selectKit");
-                        var option = document.createElement("option");
-                        option.text = kitname;
-                        //obtenemos el valor de la option para poderlo meter en el array de preseleccion///
-                        option.value = valorKitname;
-                        x.appendChild(option);
-                       // console.log("ID - " + valorKitname + " >  Kitname - " + kitname);
+            var valorKitname = kitjson[i].id;
+            var kitname = kitjson[i].kitname;
+            var x = document.getElementById("selectKit");
+            var option = document.createElement("option");
+            option.text = kitname;
+            //obtenemos el valor de la option para poderlo meter en el array de preseleccion///
+            option.value = valorKitname;
+            x.appendChild(option);
+            // console.log("ID - " + valorKitname + " >  Kitname - " + kitname);
 
-                    } 
-                });
+        }
+    });
 
     /////render necesario para refrescar el select //////
 
-        var mySelect = $('#selectKit');
+    var mySelect = $('#selectKit');
 
-                   $('#btn-render').on('click', function() {
-                  
-                   // alert("render del select dinamico");
-                   // mySelect.find('option:selected').prop('disabled', true);
-                    mySelect.find('option:selected');
-                    mySelect.selectpicker('refresh');
-                    mySelect.selectpicker('render');
-                    
-               });
-               
+    $('#btn-render').on('click', function () {
+
+        // alert("render del select dinamico");
+        // mySelect.find('option:selected').prop('disabled', true);
+        mySelect.find('option:selected');
+        mySelect.selectpicker('refresh');
+        mySelect.selectpicker('render');
+
+    });
+
     ////// funcion para leer un archivo via remoto http ////////
 
-   /* $(function () {
-        $("#button").click(function () {
-            alert('boton clickeado');
-            //       var TXT_URL = 'https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt';
-            var TXT_URL = $("#input-url").val();
+    /* $(function () {
+         $("#button").click(function () {
+             alert('boton clickeado');
+             //       var TXT_URL = 'https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt';
+             var TXT_URL = $("#input-url").val();
 
-            $.ajax({
-                url: TXT_URL,
-                dataType: "text",
-                success: function (data) {
-                    $(".text").html("<pre>" + data + "</pre>");
-                }
-            });
-        });
-    });*/
+             $.ajax({
+                 url: TXT_URL,
+                 dataType: "text",
+                 success: function (data) {
+                     $(".text").html("<pre>" + data + "</pre>");
+                 }
+             });
+         });
+     });*/
 
     //// Leer un archivo TXT Local (usado para leer csv y bed en input)/////////
 
     function leerArchivo(e) {
-        
+
         var archivo = e.target.files[0];
         if (!archivo) {
             return;
@@ -74,17 +90,15 @@ $(document).ready(function () {
     function mostrarContenido(contenido) {
         var elemento = document.getElementById('input-csv');
         elemento.innerHTML = contenido;
-    //    alert("File content:  " + contenido);
+        //    alert("File content:  " + contenido);
         document.getElementById('input-csv').value = contenido;
 
     }
 
-        document.getElementById('file-input')
+    document.getElementById('file-input')
         .addEventListener('change', leerArchivo, false);
 
     ////////////////////////////////////////////////////
 
 
 }); ////cierre documentReady////////
-
-
